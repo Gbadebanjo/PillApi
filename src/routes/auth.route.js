1182f644a5b81ac20a44aaffdade73111aee84e7
Afoimpact <afoimpact@gmail.com>
2023-10-09 10:23:19 +0100

    route change on auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,9 +5,9 @@ const { validateReq } = require("../middleware/validate");
 const { login, signUp } = require("../validations/auth.validations");
 const router = express.Router();
 
-router.get("/sign-in", validateReq(login), (new UserController()).getAllUser);
+router.post("/sign-in", validateReq(login), (new UserController()).getAllUser);
 
-router.get("/sign-up", validateReq(signUp), (new UserController()).getAllUser);
+router.post("/sign-up", validateReq(signUp), (new UserController()).getAllUser);
 
 
 module.exports = router;
